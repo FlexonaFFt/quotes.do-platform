64fb555cf2c6d37f9db83fdc71a0e1f62eb11dc5
FlexonaFFt <flexonafft@bk.ru>
2024-07-24 10:11:11 +0300

    --load spline-model components

---
+++ front/src/app/components/spline-main-model.js
@@ -0,0 +1,12 @@
+import Spline from '@splinetool/react-spline/next';
+import '../styles/spline-main-model.css';
+
+export default function HomeSplineModel() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/7GbN4xPBWypRgkhs/scene.splinecode" 
+      />
+    </main>
+  );
+}
--- front/src/app/page.js
+++ front/src/app/page.js
@@ -1,35 +1,8 @@
 'use client';
-import { useRef } from 'react';
-import Spline from '@splinetool/react-spline';
+import HomeSplineModel from "./components/spline-main-model";
 
-export default function SplineModel() {
-  const cube = useRef();
-
-  function onLoad(spline) {
-    const obj = spline.findObjectByName('Cube');
-    // or
-    // const obj = spline.findObjectById('8E8C2DDD-18B6-4C54-861D-7ED2519DE20E');
-
-    // save it in a ref for later use
-    cube.current = obj;
-  }
-
-  function moveObj() {
-    console.log(cube.current); // Spline Object => { name: 'Cube', id: '8E8C2DDD-18B6-4C54-861D-7ED2519DE20E', position: {}, ... }
-
-    // move the object in 3D space
-    cube.current.position.x += 10;
-  }
-
+export default function App() {
   return (
-    <div>
-      <Spline
-        scene="https://prod.spline.design/6Wq1Q7YGyM-iab9i/scene.splinecode"
-        onLoad={onLoad}
-      />
-      <button type="button" onClick={moveObj}>
-        Move Cube
-      </button>
-    </div>
+    <HomeSplineModel />
   );
 }
